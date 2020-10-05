c889f2efb3966ff47f99bb92c6c20d4bd7771d0f
JimmyLiu <jimmy021016@gmail.com>
2020-10-05 11:29:32 +0800

    Fix string separating problem

--- perf.c
+++ perf.c
@@ -10,12 +10,10 @@
 
 int no_bloom_filter(const tst_node *root, char *out_file)
 {
-    char word[WORDMAX] = "";
     FILE *fp = fopen(out_file, "w");
     FILE *dict = fopen(DICT_FILE, "r");
     int idx = 0;
     double t1, t2;
-    tst_node *res = NULL;
 
     if (!fp || !dict) {
         if (fp) {
@@ -29,12 +27,23 @@ int no_bloom_filter(const tst_node *root, char *out_file)
         return 1;
     }
 
-    while (fscanf(dict, "%s", word) != EOF) {
-        t1 = tvgetf();
-        res = tst_search(root, word);
-        t2 = tvgetf();
-        fprintf(fp, "%d %f msec\n", idx, (t2 - t1) * 1000);
-        idx++;
+    char buf[WORDMAX];
+    char str[WORDMAX];
+    int stridx = 0;
+    while (fgets(buf, WORDMAX, dict)) {
+        for (int i = 0; i < strlen(buf); i++) {
+            if (buf[i] == ',' || buf[i] == '\n') {
+                str[stridx] = '\0';
+                i++;
+                stridx = 0;
+                t1 = tvgetf();
+                tst_search(root, str);
+                t2 = tvgetf();
+                fprintf(fp, "%d %f msec\n", idx, (t2 - t1) * 1000);
+                idx++;
+            } else
+                str[stridx++] = buf[i];
+        }
     }
 
     fclose(fp);
@@ -44,12 +53,10 @@ int no_bloom_filter(const tst_node *root, char *out_file)
 
 int bloom_filter(const tst_node *root, char *out_file, bloom_t bloom)
 {
-    char word[WORDMAX] = "";
     FILE *fp = fopen(out_file, "w");
     FILE *dict = fopen(DICT_FILE, "r");
     int idx = 0;
     double t1, t2;
-    tst_node *res = NULL;
 
     if (!fp || !dict) {
         if (fp) {
@@ -63,19 +70,31 @@ int bloom_filter(const tst_node *root, char *out_file, bloom_t bloom)
         return 1;
     }
 
-    while (fscanf(dict, "%s", word) != EOF) {
-        t1 = tvgetf();
-        if (!bloom_test(bloom, word)) { /* check if word is in bloom filter
-                                           in advance*/
-            t2 = tvgetf();
-            fprintf(fp, "%d %f msec\n", idx, (t2 - t1) * 1000);
-            idx++;
-            continue;
+    char buf[WORDMAX];
+    char str[WORDMAX];
+    int stridx = 0;
+    while (fgets(buf, WORDMAX, dict)) {
+        for (int i = 0; i < strlen(buf); i++) {
+            if (buf[i] == ',' || buf[i] == '\n') {
+                str[stridx] = '\0';
+                i++;
+                stridx = 0;
+                t1 = tvgetf();
+                if (!bloom_test(
+                        bloom,
+                        str)) { /* check if word is in bloom filter in advance*/
+                    t2 = tvgetf();
+                    fprintf(fp, "%d %f msec\n", idx, (t2 - t1) * 1000);
+                    idx++;
+                    continue;
+                }
+                tst_search(root, str);
+                t2 = tvgetf();
+                fprintf(fp, "%d %f msec\n", idx, (t2 - t1) * 1000);
+                idx++;
+            } else
+                str[stridx++] = buf[i];
         }
-        res = tst_search(root, word);
-        t2 = tvgetf();
-        fprintf(fp, "%d %f msec\n", idx, (t2 - t1) * 1000);
-        idx++;
     }
 
     fclose(fp);
